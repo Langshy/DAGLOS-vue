4f14a684ff85470811caa3158dce8aab884a001e
lanshy <langshy@163.com>
2024-12-10 10:03:22 +0800

    添加备注，添加界面暗色配色

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 // main.ts
 import { createApp } from 'vue'
+//导入 element puls 组件
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+//设置中文
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+//设置界面暗黑色
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './styles/dark/css-vars.css'
+
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(ElementPlus, {
+    locale: zhCn,
+  })
 app.mount('#app')
